d506c52642f3a0014ba27b62f2fbe65cb375f5e6
SirAlchemer <120516112+SirAlchemer@users.noreply.github.com>
2024-10-12 16:34:46 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='func',
+    version='1.0.0',
+    packages=['func'],
+    install_requires=[],
+    author='Your Name',
+    author_email='your_email@example.com'
+)
