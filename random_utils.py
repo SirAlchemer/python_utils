2c2d338e72ba10083bb3bcd70b46dafe6cfe7954
SirAlchemer <120516112+SirAlchemer@users.noreply.github.com>
2024-10-28 08:21:28 -0600

    Update random_utils.py

--- random_utils.py
+++ random_utils.py
@@ -3,7 +3,7 @@ import random
 
 # ______random_slot_setter_______#
 def random_slot_setter(number_of_slots, posible_rolls):
-    machine = [None] * number_of_slots
+    slot = [None] * number_of_slots
     for i in range(len(machine)):
-        machine[i] = random.choice(posible_rolls)
-    return machine
+        slot[i] = random.choice(posible_rolls)
+    return slot
