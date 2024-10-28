85b01945cc1a47ab9a9dc84fcac7d50216cc8764
SirAlchemer <120516112+SirAlchemer@users.noreply.github.com>
2024-10-28 08:17:49 -0600

    Update error_utils.py

--- error_utils.py
+++ error_utils.py
@@ -1,7 +1,13 @@
+import logging
 
 
-def raise_error(ErrorName: str, message: str):
-    if isinstance(ErrorName, str):
+def raise_error(ErrorName: str, message: str, log_error: str = None) -> None:
+    if log_error != None:
         CustomError = type(ErrorName, (Exception,), {})
+        logging(log_error)
         raise CustomError(message)
+    if not isinstance(ErrorName, str):
+        raise ValueError(f'{ErrorName} is not a string!')
+    CustomError = type(ErrorName, (Exception,), {})
+    raise CustomError(message)
 
