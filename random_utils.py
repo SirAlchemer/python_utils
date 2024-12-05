0a7f1f85951e7be62c686b6e9c83899bdeed4524
SirAlchemer <120516112+SirAlchemer@users.noreply.github.com>
2024-12-04 18:48:04 -0700

    Delete random_utils.py

--- random_utils.py
+++
@@ -1,9 +0,0 @@
-import random
-
-
-# ______random_slot_setter_______#
-def random_slot_setter(number_of_slots, posible_rolls):
-    slot = [None] * number_of_slots
-    for i in range(len(machine)):
-        slot[i] = random.choice(posible_rolls)
-    return slot
