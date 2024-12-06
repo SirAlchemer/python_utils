35c4c52336170ad31e32628e7df2e0a1d84980d1
SirAlchemer <120516112+SirAlchemer@users.noreply.github.com>
2024-12-05 18:37:40 -0700

    Update input_utils.py

--- input_utils.py
+++ input_utils.py
@@ -1,29 +1,37 @@
+def get_attr(
+    prompt, data_type=str, min_int: int | None = None, max_int: int | None = None,
+    custom_error: str | None = None):
+    validation_functions = {
+        int: lambda x: (min_int is None or x >= min_int) and (max_int is None or x <= max_int),
+        float: lambda x: True,
+        bool: lambda x: x in [True, False],
+        str: lambda x: True
+    }
 
+    error_messages = {
+        int: lambda x: f"{x} is not a valid integer",
+        float: lambda x: f"{x} is not a valid number",
+        bool: lambda x: f"{x} is not 'Y' or 'N'",
+        str: lambda x: ""
+    }
 
-def get_attr(prompt, data_type = str):
     while True:
         response = input(prompt)
-        if data_type is int:
-            try:
-                response = int(response)
-                return response
-            except ValueError:
-                print(f"{response} is not a valid intiger")
-        elif data_type is float:
-            try:
-                response = float(response)
-                return response
-            except ValueError:
-                print(f"{response} is not a valid number")
-        elif data_type is bool:
-            try:
-                if response.lower() == "y" or response.lower() == "[y]":
-                    return True
-                elif response.lower() == "n" or response.lower() == "[n]":
-                    return False
+        try:
+            response = data_type(response)
+            if not validation_functions[data_type](response):
+                if custom_error:
+                    print(custom_error)
+                elif min_int is not None and response < min_int:
+                    print(f"{response} is less than {min_int}!")
+                elif max_int is not None and response > max_int:
+                    print(f"{response} is greater than {max_int}!")
                 else:
-                    raise ValueError()
-            except ValueError:
-                print(f"{response} is not 'Y' or 'N'")
-        else:
-            return response
+                    print(error_messages[data_type](response))
+            else:
+                return response
+        except ValueError:
+            if custom_error:
+                print(custom_error)
+            else:
+                print(error_messages[data_type](response))
