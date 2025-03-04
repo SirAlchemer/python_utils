d8b957e2ea51d38358fab89ca30ec68df9888657
SirAlchemer <120516112+SirAlchemer@users.noreply.github.com>
2025-03-03 18:04:38 -0700

    Create util_classes.py

---
+++ util_classes.py
@@ -0,0 +1,13 @@
+
+
+class Chars:
+    LOWER_ALPHA = 'abcdefghijklmnopqrstuvwxyz'
+    UPPER_ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    MIXED_ALPHA = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    LOWER_ALPHA_NUMERIC = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    UPPER_ALPHA_NUMERIC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    MIXED_ALPHA_NUMERIC = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    NUMERIC = '0123456789'
+    MISC = r'`~@#$%^&*()_-+={[}]|\;:"<,>.?/' + "'"
+    ALL = r'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`~@#$%^&*()_-+={[}]|\;:"<,>.?/' + "'"
+
