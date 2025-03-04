5e815df7602269c9a0561faf17e63323c1590d87
SirAlchemer <120516112+SirAlchemer@users.noreply.github.com>
2025-03-03 20:38:13 -0700

    Update format_utils.py

--- format_utils.py
+++ format_utils.py
@@ -1,66 +1,66 @@
-from .util_classes import Color   
+from .util_classes import ANSI_Codes   
     
 def ANSI(string: str, **kwargs) -> str:
     if kwargs.get("red", False) == True:
-        string = Color.RED + string
+        string = ANSI_Codes.RED + string
     if kwargs.get("cyan", False) == True:
-        string = Color.CYAN + string
+        string = ANSI_Codes.CYAN + string
     if kwargs.get("add_end_ansi", True) == False: 
         return string
     if kwargs.get("bold", False) == True:
-        string = Color.BOLD + string
+        string = ANSI_Codes.BOLD + string
     if kwargs.get("dark_gray", False) == True:
-        string = Color.DARK_GRAY + string
+        string = ANSI_Codes.DARK_GRAY + string
     if kwargs.get("italics", False) == True:
-        string = Color.ITALICS + string
+        string = ANSI_Codes.ITALICS + string
     if kwargs.get("underline", False) == True:
-        string = Color.UNDERLINE + string
+        string = ANSI_Codes.UNDERLINE + string
     if kwargs.get("white_background", False) == True:
-        string = Color.WHITE_BACKGROUND + string
+        string = ANSI_Codes.WHITE_BACKGROUND + string
     if kwargs.get("black", False) == True:
-        string = Color.BLACK + string
+        string = ANSI_Codes.BLACK + string
     if kwargs.get("strike_through", False) == True:
-        string = Color.STRIKE_THROUGH + string
+        string = ANSI_Codes.STRIKE_THROUGH + string
     if kwargs.get("double_underline", False) == True:
-        string = Color.DOUBLE_UNDERLINE + string
+        string = ANSI_Codes.DOUBLE_UNDERLINE + string
     if kwargs.get("red_background", False) == True:
-        string = Color.RED_BACKGROUND + string
+        string = ANSI_Codes.RED_BACKGROUND + string
     if kwargs.get("mint_background", False) == True:
-        string = Color.MINT_BACKGROUND + string
+        string = ANSI_Codes.MINT_BACKGROUND + string
     if kwargs.get("yellow_background", False) == True:
-        string = Color.YELLOW_BACKGROUND + string
+        string = ANSI_Codes.YELLOW_BACKGROUND + string
     if kwargs.get("blue_background", False) == True:
-        string = Color.BlUE_BACKGROUND + string
+        string = ANSI_Codes.BlUE_BACKGROUND + string
     if kwargs.get("magenta_background", False) == True:
-        string = Color.MAGENTA_BACKGROUND + string
+        string = ANSI_Codes.MAGENTA_BACKGROUND + string
     if kwargs.get("cyan_background", False) == True:
-        string = Color.CYAN_BACKGROUND + string
+        string = ANSI_Codes.CYAN_BACKGROUND + string
     if kwargs.get("white_background", False) == True:
-        string = Color.WHITE_BACKGROUND + string
+        string = ANSI_Codes.WHITE_BACKGROUND + string
     if kwargs.get("overline", False) == True:
-        string = Color.OVER_LINE + string
+        string = ANSI_Codes.OVER_LINE + string
     if kwargs.get("light_red", False) == True:
-        string = Color.LIGHT_RED + string
+        string = ANSI_Codes.LIGHT_RED + string
     if kwargs.get("gray_background", False) == True:
-        string = Color.GRAY_BACKGROUND + string
+        string = ANSI_Codes.GRAY_BACKGROUND + string
     if kwargs.get("light_red_background", False) == True:
-        string = Color.LIGHT_RED_BACKGROUND + string
+        string = ANSI_Codes.LIGHT_RED_BACKGROUND + string
     if kwargs.get("gray", False) == True:
-        string = Color.GRAY + string
+        string = ANSI_Codes.GRAY + string
     if kwargs.get("mint", False) == True:
-        string = Color.MINT + string
+        string = ANSI_Codes.MINT + string
     if kwargs.get("yellow", False) == True:
-        string = Color.YELLOW + string
+        string = ANSI_Codes.YELLOW + string
     if kwargs.get("magenta", False) == True:
-        string = Color.MAGENTA + string
+        string = ANSI_Codes.MAGENTA + string
     if kwargs.get("white", False) == True:
-        string = Color.WHITE + string
+        string = ANSI_Codes.WHITE + string
     if kwargs.get("light_red", False) == True:
-        string = Color.LIGHT_RED + string
+        string = ANSI_Codes.LIGHT_RED + string
     if kwargs.get("light_red_background", False) == True:
-        string = Color.LIGHT_RED_BACKGROUND + string
+        string = ANSI_Codes.LIGHT_RED_BACKGROUND + string
     if kwargs.get("gray_background", False) == True:
-        string = Color.GRAY_BACKGROUND + string
+        string = ANSI_Codes.GRAY_BACKGROUND + string
     if kwargs.get("light_blue", False) == True:
-        string = Color.CYAN + string
-    return string + Color.END_ANSI
+        string = ANSI_Codes.CYAN + string
+    return string + ANSI_Codes.END_ANSI
