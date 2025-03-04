8e40c47b8f4c918d883c95f9fb1be9e181586a60
SirAlchemer <120516112+SirAlchemer@users.noreply.github.com>
2025-03-03 19:44:20 -0700

    Update util_classes.py

--- util_classes.py
+++ util_classes.py
@@ -1,5 +1,34 @@
 
 
+class Color:
+    END_ANSI = "\033[0m"
+    BOLD = '\033[1m'
+    DARK_GRAY = '\033[2m'
+    ITALICS = '\033[3m'
+    UNDERLINE = '\033[4m'
+    WHITE_BACKGROUND = '\033[7m'
+    BLACK = '\033[8m'
+    STRIKE_THROUGH = '\033[9m'
+    DOUBLE_UNDERLINE = '\033[21m'
+    RED = '\033[31m'
+    GRAY = '\033[30m'
+    MINT = '\033[32m'
+    YELLOW = '\033[33m'
+    CYAN = '\033[34m'
+    MAGENTA = '\033[35m'
+    WHITE = '\033[37m'
+    RED_BACKGROUND = '\033[41m'
+    MINT_BACKGROUND = '\033[42m'
+    YELLOW_BACKGROUND = '\033[43m'
+    BlUE_BACKGROUND = '\033[44m'
+    MAGENTA_BACKGROUND = '\033[45m'
+    CYAN_BACKGROUND = '\033[46m'
+    WHITE_BACKGROUND = '\033[47m'
+    OVER_LINE = '\033[53m'
+    LIGHT_RED = "\033[91m"
+    GRAY_BACKGROUND = "\033[100m"
+    LIGHT_RED_BACKGROUND = "\033[101m"
+
 class Chars:
     LOWER_ALPHA = 'abcdefghijklmnopqrstuvwxyz'
     UPPER_ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
