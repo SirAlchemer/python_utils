19ca2ee4cedbc1df999e196f6e36668e30c81058
SirAlchemer <120516112+SirAlchemer@users.noreply.github.com>
2025-05-20 12:17:39 -0600

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,20 +1,4 @@
 
-class Dicts:
-    # Create mappings for lowercase a-z
-    char_to_num = {chr(i): i - 96 for i in range(97, 123)}
-    # Add mappings for uppercase A-Z
-    char_to_num.update({chr(i): i - 64 for i in range(65, 91)})
-    # Add mappings for special characters
-    special_chars = "!@#$%^&*()-_=+[]{}|;:',.<>?/~`"
-    char_to_num.update({char: idx + 53 for idx, char in enumerate(special_chars)})  # Start numbering after 52
-
-    # Create mappings for numbers 1-26 to lowercase a-z
-    num_to_char = {i: chr(i + 96) for i in range(1, 27)}
-    # Add mappings for numbers 1-26 to uppercase A-Z
-    num_to_char.update({i + 26: chr(i + 64) for i in range(1, 27)})
-    # Add mappings for special characters
-    num_to_char.update({idx + 53: char for idx, char in enumerate(special_chars)})
-
 def middle_lexicographical_permutation(string):
     string = sorted(string)
     n = len(string)
